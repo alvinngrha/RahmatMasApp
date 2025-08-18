454dd299b4e7ec234f6908f7bd68806d21746539
alvinngrha <alvinnugraha500@gmail.com>
2025-08-18 14:11:22 +0800

    refactor: supabasemodule

--- app/src/main/java/com/example/rahmatmas/data/supabase/SupabaseModule.kt
+++ app/src/main/java/com/example/rahmatmas/data/supabase/SupabaseModule.kt
@@ -1,5 +1,6 @@
 package com.example.rahmatmas.data.supabase
 
+import com.example.rahmatmas.BuildConfig
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.auth.Auth
 import io.github.jan.supabase.createSupabaseClient
@@ -9,8 +10,8 @@ import io.github.jan.supabase.storage.Storage
 
 object SupabaseModule {
     val client: SupabaseClient = createSupabaseClient(
-        supabaseUrl = "https://awhyvidcoelagcgwkqks.supabase.co", // Ganti dengan URL Supabase Anda
-        supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF3aHl2aWRjb2VsYWdjZ3drcWtzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIyMTM0MjUsImV4cCI6MjA2Nzc4OTQyNX0.jVTeO5bBkjcI5vRc_48UpViYLNiOt50TjKRTqQ51Y-c" // Ganti dengan Anon Key Supabase Anda
+        supabaseUrl = BuildConfig.SUPABASE_URL, // Ganti dengan URL Supabase Anda
+        supabaseKey = BuildConfig.SUPABASE_ANON_KEY // Ganti dengan Anon Key Supabase Anda
     ) {
         install(Auth)
         install(Postgrest)
