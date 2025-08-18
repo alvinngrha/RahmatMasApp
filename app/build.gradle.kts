791042bcde9746c9ce622ed3fc7180ba129157a7
alvinngrha <alvinnugraha500@gmail.com>
2025-08-18 14:01:18 +0800

    fix: catalog costumer

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,18 @@ android {
             "\"https://logam-mulia-api.vercel.app/\""
         )
 
+        buildConfigField(
+            "String",
+            "SUPABASE_URL",
+            "\"https://awhyvidcoelagcgwkqks.supabase.co\""
+        )
+
+        buildConfigField(
+            "String",
+            "SUPABASE_ANON_KEY",
+            "\"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF3aHl2aWRjb2VsYWdjZ3drcWtzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIyMTM0MjUsImV4cCI6MjA2Nzc4OTQyNX0.jVTeO5bBkjcI5vRc_48UpViYLNiOt50TjKRTqQ51Y-c\""
+        )
+
     }
 
     buildTypes {
